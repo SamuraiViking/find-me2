8a445be9d29828f90b28bdf144654f879a7655c5
SamuraiViking <nelson67@stolaf.edu>
2019-11-10 15:44:29 -0600

    add createTablesLegend function

--- src/CafFirstFloor.js
+++ src/CafFirstFloor.js
@@ -2,100 +2,72 @@ import React from 'react';
 import './App.css';
 
 class CafFirstFloor extends React.Component {
+    createSquareRectangleBoothTables = () => {
+        let tables = []
+        let tablesOrder = ["sq", "long", "long", "aisle", "long", "long", "sq","booth"]
+        for(let i = 0; i < 6; i++) {
+            let tableRow = []
+            tablesOrder.forEach((tableType) => {
+                tableRow.push(<div className={tableType}></div>)
+            })
+            tables.push(tableRow)
+        }
+        return tables
+    }
+    createCircleTables = () => {
+        let tables = []
+        let tablesOrder = [
+            ['', 'circle', '', 'circle', '', 'circle', '', ''],
+            ['', '', 'circle', '', 'circle', '', '', '']
+        ]
+        tablesOrder.forEach((tableRowOrder) => {
+            tableRowOrder.forEach((tableType) => {
+                tables.push(<div className={tableType}></div>)
+            })
+        })
+        return tables
+    }
+    createTablesLegend = () => {
+        let legend = []
+        let legendOrder = [
+            {
+                "tableType": "sq",
+                "name": "Two Person Table"
+            },
+            {
+                "tableType": "long",
+                "name": "Long Table"
+            },
+            {
+                "tableType": "booth",
+                "name": "Booth Tables"
+            },
+            {
+                "tableType": "circle",
+                "name": "Circular Tables"
+            }
+        ]
+        legendOrder.forEach((elem) => {
+            legend.push(<div className={elem.tableType}></div>)
+            legend.push(<div className="label">{`${elem.name}`}</div>)
+        })
+        return legend
+    }
     render() {
         const rows = 6;
+
+
         return (
             <div>
                 <div class="header">
                     <h1>Tap your Table</h1>
                 </div>
                 <div className="caf-first-floor">
-                    <div></div>
-                    <div className="circle"></div>
-                    <div></div>
-                    <div className="circle"></div>
-                    <div></div>
-                    <div className="circle"></div>
-                    <div></div>
-                    <div></div>
-
-                    <div></div>
-                    <div></div>
-                    <div className="circle table"></div>
-                    <div></div>
-                    <div className="circle table"></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-
+                    {this.createCircleTables()}
                     <div className="blank-row"></div>
-
-                    <div className="sq table"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="aisle"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="sq table"></div>
-                    <div className="booth table"></div>
-
-                    <div className="sq table"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="aisle"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="sq table"></div>
-                    <div className="booth table"></div>
-
-                    <div className="sq table"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="aisle"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="sq table"></div>
-                    <div className="booth table"></div>
-
-                    <div className="sq table"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="aisle"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="sq table"></div>
-                    <div className="booth table"></div>
-
-                    <div className="sq table"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="aisle"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="sq table"></div>
-                    <div className="booth table"></div>
-
-                    <div className="sq table"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="aisle"></div>
-                    <div className="long table"></div>
-                    <div className="long table"></div>
-                    <div className="sq table"></div>
-                    <div className="booth table"></div>
-
+                    {this.createSquareRectangleBoothTables()}
                     <div className="blank-row"></div>
-                    <div className="sq table"></div>
-                    <div className="label">Two Person Table</div>
-
-                    <div className="long table"></div>
-                    <div className="label">Long Table</div>
-                    <div className="booth"></div>
-                    <div className="label">Booth Table</div>
-                    <div className="circle"></div>
-                    <div className="label">Circle Table</div>
-
-
+                    {this.createTablesLegend()}
                 </div>
             </div>
         );
