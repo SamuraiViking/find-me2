6aaab681bd32819a562d604f7bf232253ebc5dba
SamuraiViking <nelson67@stolaf.edu>
2019-11-10 14:53:06 -0600

    format tables

---
+++ src/CafFirstFloor.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import './App.css';
+
+class CafFirstFloor extends React.Component {
+    render() {
+        const rows = 6;
+        return (
+            <div>
+                <div class="header">
+                    <h1>Tap your Table</h1>
+                </div>
+                <div className="caf-first-floor">
+                    <div></div>
+                    <div className="circle"></div>
+                    <div></div>
+                    <div className="circle"></div>
+                    <div></div>
+                    <div className="circle"></div>
+                    <div></div>
+                    <div></div>
+
+                    <div></div>
+                    <div></div>
+                    <div className="circle table"></div>
+                    <div></div>
+                    <div className="circle table"></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+
+                    <div className="blank-row"></div>
+
+                    <div className="sq table"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="aisle"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="sq table"></div>
+                    <div className="booth table"></div>
+
+                    <div className="sq table"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="aisle"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="sq table"></div>
+                    <div className="booth table"></div>
+
+                    <div className="sq table"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="aisle"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="sq table"></div>
+                    <div className="booth table"></div>
+
+                    <div className="sq table"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="aisle"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="sq table"></div>
+                    <div className="booth table"></div>
+
+                    <div className="sq table"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="aisle"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="sq table"></div>
+                    <div className="booth table"></div>
+
+                    <div className="sq table"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="aisle"></div>
+                    <div className="long table"></div>
+                    <div className="long table"></div>
+                    <div className="sq table"></div>
+                    <div className="booth table"></div>
+
+                    <div className="blank-row"></div>
+                    <div className="sq table"></div>
+                    <div className="label">Two Person Table</div>
+
+                    <div className="long table"></div>
+                    <div className="label">Long Table</div>
+                    <div className="booth"></div>
+                    <div className="label">Booth Table</div>
+                    <div className="circle"></div>
+                    <div className="label">Circle Table</div>
+
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CafFirstFloor;
