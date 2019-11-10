cebacb3b3586c223c6809363dcab63e56ac611ec
SamuraiViking <nelson67@stolaf.edu>
2019-11-10 16:30:32 -0600

    add click event

--- src/CafFirstFloor.js
+++ src/CafFirstFloor.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 
 class CafFirstFloor extends React.Component {
-    handleClick() {
-        console.log("you clicked on me");
+    handleClick(key, event) {
+        console.log(key);
     }
     createSquareRectangleBoothTables = () => {
         let tables = []
@@ -11,10 +11,11 @@ class CafFirstFloor extends React.Component {
         for(let i = 0; i < 6; i++) {
             let tableRow = []
             tablesOrder.forEach((tableType, index) => {
+                let key = `${i}${index}${tableType}`;
                 tableRow.push(<div 
                     className={tableType} 
-                    onClick={() => this.handleClick()}
-                    key={`${i}${index}${tableType}`}>
+                    onClick={this.handleClick.bind(this, key)}
+                    key={key}>
                     </div>)
             })
             tables.push(tableRow)
@@ -29,9 +30,10 @@ class CafFirstFloor extends React.Component {
         ]
         tablesOrder.forEach((tableRowOrder, index1) => {
             tableRowOrder.forEach((tableType, index2) => {
+                let key = `${index1}${index2}${tableType}`
                 tables.push(<div 
                     className={tableType}
-                    key={`${index1}${index2}${tableType}`}>
+                    key={key}>
                     </div>)
             })
         })
@@ -58,8 +60,10 @@ class CafFirstFloor extends React.Component {
             }
         ]
         legendOrder.forEach((elem, index) => {
-            legend.push(<div className={elem.tableType} key={`${index}${elem.tableType}`}></div>)
-            legend.push(<div className="label" key={`${index}${elem.name}`}>{`${elem.name}`}</div>)
+            let legendTableKey = `${index}${elem.tableType}`
+            let legendLabelKey = `${index}${elem.name}${elem.name}`
+            legend.push(<div className={elem.tableType} key={legendTableKey}></div>)
+        legend.push(<div className="label" key={legendLabelKey}>{elem.name}</div>)
         })
         return legend
     }
